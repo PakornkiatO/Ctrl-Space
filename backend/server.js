bf3915f2af55530ddb25f4e5e787c73a26bb2509
Pakornkiat Opaprakasit <piakpond@Pakornkiats-MacBook-Air.local>
2025-04-12 20:02:36 +0700

    Use param from .env file

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config({path: './config/config.env'});
 
 const app = express();
 
 app.use('/api/v1/coworkings', require('./routes/coworkings.js'));
 
-app.listen(5003, console.log('Server running on port 5003'));
+const PORT = process.env.PORT;
+app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' on port ', PORT));
