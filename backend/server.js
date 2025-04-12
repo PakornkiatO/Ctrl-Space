026ac10e28cafea6f682cc92084b39ca7174fd22
Pakornkiat Opaprakasit <piakpond@Pakornkiats-MacBook-Air.local>
2025-04-12 20:41:12 +0700

    Connect to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db.js');
 
 dotenv.config({path: './config/config.env'});
+
+connectDB();
 
 const app = express();
 
 app.use('/api/v1/coworkings', require('./routes/coworkings.js'));
 
 const PORT = process.env.PORT;
-app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' on port ', PORT));
+const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' on port ', PORT));
+
+process.on('unhandleRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+
+    server.close(() => process.exit(1));
+});
