8cf5bac96b9075b9ef29289b57d8c5c6a9e4e0c6
Pakornkiat Opaprakasit <piakpond@Pakornkiats-MacBook-Air.local>
2025-04-16 14:15:19 +0700

    Add login function

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -21,3 +21,21 @@ exports.register = async (req, res, next) => {
         console.log(err);
     }
 }
+
+exports.login = async (req, res, next) => {
+    const {email, password} = req.body;
+
+    if(!email || !password) return res.status(400).json({success: false, msg: 'Please provide an email and password'});
+
+    const user = await User.findOne({email}).select('+password');
+
+    if(!user) return res.status(400).json({success: false, msg: 'Invalid credentials'});
+
+    const isMatch = user.matchPassword(password);
+
+    if(!isMatch) return res.status(401).json({success: false, msg: 'Invalid credentials'});
+
+    const token = user.getSignedJwtToken();
+
+    res.status(200).json({success: true, token: token});
+}
--- backend/models/User.js
+++ backend/models/User.js
@@ -48,4 +48,8 @@ UserSchema.methods.getSignedJwtToken = function(){
     return jwt.sign({id: this.id}, process.env.JWT_SECRET, {expiresIn: process.env.JWT_EXPIRE});
 }
 
+UserSchema.methods.matchPassword = async function(enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+}
+
 module.exports = mongoose.model('User', UserSchema);
