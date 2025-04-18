ae057897731d46b402689c203a95879fb967c241
Pakornkiat Opaprakasit <piakpond@Pakornkiats-MacBook-Air.local>
2025-04-18 11:57:35 +0700

    Add authorize function

--- backend/routes/coworkings.js
+++ backend/routes/coworkings.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const {protect} = require('../middleware/auth');
+const {protect, authorize} = require('../middleware/auth');
 const {getCoworkings, getCoworking, createCoworking, updateCoworking, deleteCoworking} = require('../controllers/coworkings');
 
-router.route('/').get(getCoworkings).post(protect, createCoworking);
-router.route('/:id').get(getCoworking).put(protect, updateCoworking).delete(protect, deleteCoworking);
+router.route('/').get(getCoworkings).post(protect, authorize('admin'), createCoworking);
+router.route('/:id').get(getCoworking).put(protect, authorize('admin'), updateCoworking).delete(protect, authorize('admin'), deleteCoworking);
 
 module.exports = router;
