87ff281c58c8df099663305487d5e2072027e2ad
Pakornkiat Opaprakasit <piakpond@Pakornkiats-MacBook-Air.local>
2025-04-19 00:16:43 +0700

    Add create reservation function

--- backend/routes/reservations.js
+++ backend/routes/reservations.js
@@ -1,10 +1,10 @@
 const express = require('express');
 
 const {protect} = require('../middleware/auth');
-const {getReservation} = require('../controllers/reservation');
+const {getReservations, addReservation} = require('../controllers/reservation');
 
-const router = express.Router();
+const router = express.Router({mergeParams: true});
 
-router.route('/').get(protect, getReservation);
+router.route('/').get(protect, getReservations).post(protect, addReservation);
 
 module.exports = router;
