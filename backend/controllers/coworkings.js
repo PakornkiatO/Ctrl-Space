c0e0fad3f74db5af651309ed14a82f04657978a8
Pakornkiat Opaprakasit <piakpond@Pakornkiats-MacBook-Air.local>
2025-04-19 14:26:31 +0700

    Add cascade delete to Co-working space controllers

--- backend/controllers/coworkings.js
+++ backend/controllers/coworkings.js
@@ -1,4 +1,5 @@
 const Coworking = require('../models/Coworking')
+const Reservation = require('../models/Reservation')
 
 exports.getCoworkings = async (req, res) => {
     // res.status(200).json({success: true, msg: `Show all Co-Working spaces`});
@@ -8,13 +9,13 @@ exports.getCoworkings = async (req, res) => {
         let query;
 
         const reqQuery = {...req.query};
-        const removeFields = ['select', 'sort'];
+        const removeFields = ['select', 'sort', 'page', 'limit'];
         removeFields.forEach(params => delete reqQuery[params]);
 
         let queryStr = JSON.stringify(req.query);
         queryStr = queryStr.replace(/\b(gt| gte| lt| lte| in)\b/g, match => `$${match}`);
 
-        query = Coworking.find(JSON.parse(queryStr));
+        query = Coworking.find(JSON.parse(queryStr)).populate('reservations');
 
         if(req.query.select) {
             const fields = req.query.select.split(',').join(' ');
@@ -25,11 +26,25 @@ exports.getCoworkings = async (req, res) => {
             const sortBy = req.query.sort.split(',').join(' ');
             query = query.sort(sortBy);
         } 
-        else query = query.sort('-createedAt');
+        else query = query.sort('-createdAt');
+
+        const page = parseInt(req.query.page, 10) || 1;
+        const limit = parseInt(req.query.limit, 10) || 25;
+        const startIndex = (page-1)*limit;
+        const endIndex = page*limit;
+        const total = await Coworking.countDocuments();
+
+        query = query.skip(startIndex).limit(limit)
 
         const coworking = await query;
 
-        res.status(200).json({success: true, data: coworking});
+        const pagination = {};
+
+        if(endIndex < total) pagination.next = {page: page+1, limit};
+
+        if(startIndex > 0) pagination.prev = {page: page-1, limit};
+
+        res.status(200).json({success: true, count: coworking.length, pagination, data: coworking});
     } catch (error) {
         res.status(400).json({success: false, msg: error});
     }
@@ -50,6 +65,17 @@ exports.updateCoworking = (req, res) => {
     res.status(200).json({success: true, msg: `Update Co-Working space ID:${req.params.id}`});
 }
 
-exports.deleteCoworking = (req, res) => {
-    res.status(200).json({success: true, msg: `Delete Co-Working space ID:${req.params.id}`});
+exports.deleteCoworking = async (req, res, next) => {
+    try {
+        const coworking = await Coworking.findById(req.params.id);
+
+        if(!coworking) return res.status(404).json({success: false, msg: `Co-working not found with id of ${req.params.id}`});
+
+        await Reservation.deleteMany({coworking: req.params.id});
+        await Coworking.deleteOne({_id: req.params.id});
+
+        res.status(200).json({success: true});
+    } catch (error) {
+        res.status(400).json({success: false, msg: `${err}`});
+    }
 }
