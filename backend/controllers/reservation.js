2dbd9c44bd98b99f15b327a60b4d676546487a52
Pakornkiat Opaprakasit <piakpond@Pakornkiats-MacBook-Air.local>
2025-04-19 21:03:46 +0700

    Done all basic requirement

--- backend/controllers/reservation.js
+++ backend/controllers/reservation.js
@@ -23,6 +23,23 @@ exports.getReservations = async (req, res, next) => {
     }
 }
 
+exports.getReservation = async (req,res,next) => {
+    try {
+        const reservation = await Reservation.findById(req.params.id).populate({
+            path: 'coworking',
+            select: 'name province tel opening_hours'
+        });
+
+        if(!reservation) return res.status(404).json({success: false, msg: `No reservation with the id of ${req.params.id}`});
+
+        res.status(200).json({success: true, data: reservation});
+    } catch (err) {
+        console.log(err);
+
+        res.status(500).json({success: false, msg: 'Cannot find reservation'});
+    }
+}
+
 exports.addReservation = async (req, res, next) => {
     try {
         req.body.coworking = req.params.coworkingId;
@@ -36,5 +53,44 @@ exports.addReservation = async (req, res, next) => {
     } catch (err) {
         console.log(err);
         res.status(500).json({success: false, msg: `Cannot create Resevation`});
+    }
+}
+
+exports.updateReservation = async (req,res,next) => {
+    try {
+        let reservation = await Reservation.findById(req.params.id);
+
+        if(!reservation) return res.status(404).json({success: false, msg: `No reservation with id of ${req.params.id}`});
+
+        if(reservation.user.toString() !== req.user.id && req.user.role !== 'admin') return res.status(401).json({success: false, msg: `User ${req.user.id} is not authorized to update this reservation`});
+
+        reservation = await Reservation.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        });
+
+        res.status(200).json({success: true, data: reservation})
+    } catch (err) {
+        console.log(err);
+
+        res.status(500).json({success: false, msg: 'Cannot update reservation'});
+    }
+}
+
+exports.deleteReservation = async (req,res,next) => {
+    try {
+        const reservation = await Reservation.findById(req.params.id);
+
+        if(!reservation) return res.status(404).json({success: false, msg: `No reservation with id of ${req.params.id}`});
+
+        if(reservation.user.toString() !== req.user.id && req.user.role !== 'admin') return res.status(401).json({success: false, msg: `User ${req.user.id} is not authorized to delete this reservation`});
+
+        await reservation.deleteOne();
+
+        res.status(200).json({success: true, data: {}});
+    } catch (err) {
+        console.log(err);
+
+        res.status(500).json({success: false, msg: 'Cannot delete reservation'});
     }
 }
--- backend/routes/reservations.js
+++ backend/routes/reservations.js
@@ -1,10 +1,11 @@
 const express = require('express');
 
-const {protect} = require('../middleware/auth');
-const {getReservations, addReservation} = require('../controllers/reservation');
+const {protect, authorize} = require('../middleware/auth');
+const {getReservations, getReservation, addReservation, updateReservation, deleteReservation} = require('../controllers/reservation');
 
 const router = express.Router({mergeParams: true});
 
-router.route('/').get(protect, getReservations).post(protect, addReservation);
+router.route('/').get(protect, getReservations).post(protect, authorize('admin', 'user'), addReservation);
+router.route('/:id').get(protect, getReservation).put(protect, authorize('admin', 'user'), updateReservation).delete(protect, authorize('admin', 'user'), deleteReservation);
 
 module.exports = router;
